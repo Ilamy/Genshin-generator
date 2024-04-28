5dad530891db2906998d21c22c13c93d020ed75c
Ilamy <168302741+Ilamy@users.noreply.github.com>
2024-04-28 21:41:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tkinter as tk
+from tkinter import *
 import customtkinter as ctk
 import random
 import sqlite3
 import os
+from PIL import Image, ImageTk
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
@@ -15,35 +17,57 @@ conn = sqlite3.connect("Genshin.db")
 
 cursor = conn.cursor()
 
+def load_image(path):
+    try:
+        with Image.open(path) as img:
+            return ImageTk.PhotoImage(img)
+    except IOError as e:
+        print(f"Error opening image file: {e}")
+        return None
+
+def update_display():
+    character = generate()
+    character_name = character.split('.')[0]
+    character_info = getCharactersInfo(character_name)
+    image_path = os.path.join(r"C:\Users\adaml\Desktop\Genshin generator\images", character)
+    photoimage = load_image(image_path)
+    
+    if photoimage: 
+        label1.config(image=photoimage)
+        label1.image = photoimage
+
 def get_file_names(folder_path):
-    # Check if the folder path exists
     if not os.path.exists(folder_path):
         print(f"Folder '{folder_path}' does not exist.")
         return []
 
-    # Get all file names in the folder
     file_names = os.listdir(folder_path)
     return file_names
 
 def generate():
-    #random number
-    character_number = random.randint(0,11)
-    #list of characters
     character_list = get_file_names(r"C:\Users\adaml\Desktop\Genshin generator\images")
-
+    character_number = random.randint(0, len(character_list) - 1)
     return character_list[character_number]
-
-character = generate().split('.')
 
 def getCharactersInfo(character):
     sql="SELECT * FROM Characters WHERE name = :name "
-    
     cursor.execute(sql, {'name': character})
     character_info = cursor.fetchone()
     return character_info
 
-characterinfo = getCharactersInfo(character[0])  
 
-#set image + give infos
+frame1 = ctk.CTkFrame(master = app, width = 450, height = 200) 
+frame1.pack(pady = 20 ) 
+  
+button1 = ctk.CTkButton(frame1,text="Generate", command=update_display) 
+button1.pack(pady=20) 
+ 
+character = generate()
+image_path = os.path.join(r"C:\Users\adaml\Desktop\Genshin generator\images", character)
+photoimage = load_image(image_path)
 
+label1 = tk.Label(app, image=photoimage if photoimage else None)
+label1.place(x=0, y=100)
+label1.image = photoimage
+    
 app.mainloop()
